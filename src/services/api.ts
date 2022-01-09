3229c83856eb0e73ca496078eb265bf852464946
Dailton Bastos <dailtonbastos@gmail.com>
2022-01-09 18:05:48 -0300

    feat(api): add refresh token

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 import { parseCookies } from 'nookies'
 
-const cookies = parseCookies()
+import { setCookie } from '~/utils/setCookies'
+
+let cookies = parseCookies()
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
@@ -10,3 +12,27 @@ export const api = axios.create({
 api.defaults.headers.common[
   'Authorization'
 ] = `Bearer ${cookies['fastfeet.token']}`
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error: AxiosError) => {
+    if (error.response?.status === 401) {
+      if (error.response.data?.code === 'token.expired') {
+        cookies = parseCookies()
+
+        const { 'fastfeet.refreshToken': refreshToken } = cookies
+
+        const response = await api.post('/refresh', {
+          refreshToken,
+        })
+
+        const { token } = response.data
+
+        setCookie('fastfeet.token', token)
+        setCookie('fastfeet.refreshToken', response.data.refreshToken)
+
+        api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+      }
+    }
+  }
+)
