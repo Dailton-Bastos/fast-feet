225fdf4441548542b105cc0e9efe2ff777e6f132
Dailton Bastos <dailtonbastos@gmail.com>
2022-01-08 23:15:57 -0300

    chore: initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Next.js | Boilerplate</title>
+      </Head>
+    </>
+  )
+}
+
+export default Home
