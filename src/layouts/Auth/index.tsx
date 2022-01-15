b0cd9b98549ff1f8e4c45f0a84a6d2291825a242
Dailton Bastos <dailtonbastos@gmail.com>
2022-01-14 21:19:05 -0300

    refactor(auth layout): add default content

--- src/layouts/Auth/index.tsx
+++ src/layouts/Auth/index.tsx
@@ -1,11 +1,44 @@
 import React from 'react'
 
+import { Flex } from '@chakra-ui/react'
+import Image from 'next/image'
+
+import logo from '../../../public/images/logo.svg'
+
 interface AuthLayoutProps {
   children: React.ReactNode
 }
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
-  return <main>{children}</main>
+  return (
+    <Flex
+      as="main"
+      alignItems={'center'}
+      height={'100vh'}
+      justifyContent={'center'}
+      width={'100vw'}
+    >
+      <Flex
+        alignItems={'center'}
+        border={'1px'}
+        borderColor={'gray.10'}
+        bg={'white'}
+        borderRadius={'5px'}
+        boxShadow={'md'}
+        flexDirection={'column'}
+        maxWidth={360}
+        px={'8'}
+        py={'16'}
+        width={'100%'}
+      >
+        <Flex alignItems={'center'} justifyContent={'center'}>
+          <Image alt="FastFeet" src={logo} height={44} width={258} />
+        </Flex>
+
+        {children}
+      </Flex>
+    </Flex>
+  )
 }
 
 export const authLayout = (page: React.ReactElement) => {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 
-import { Flex, Box, Button, Stack, FormControl } from '@chakra-ui/react'
+import { Box, Button, Stack, FormControl } from '@chakra-ui/react'
 import { yupResolver } from '@hookform/resolvers/yup'
 import type { GetServerSideProps } from 'next'
-import Image from 'next/image'
 
 import { Input } from '~/components/Form/Input'
 import { Head } from '~/components/Head'
@@ -36,71 +35,41 @@ const SignIn: NextPageWithLayout = () => {
     <>
       <Head title="Login" description="FastFeet | Autenticação" />
 
-      <Flex
-        alignItems={'center'}
-        height={'100vh'}
-        justifyContent={'center'}
-        width={'100vw'}
-      >
-        <Flex
-          alignItems={'center'}
-          border={'1px'}
-          borderColor={'gray.10'}
-          bg={'white'}
-          borderRadius={'5px'}
-          boxShadow={'md'}
-          flexDirection={'column'}
-          maxWidth={360}
-          px={'8'}
-          py={'16'}
-          width={'100%'}
-        >
-          <Flex alignItems={'center'} justifyContent={'center'}>
-            <Image
-              alt="FastFeet"
-              height={44}
-              src="/images/logo.png"
-              width={258}
+      <Box as={'form'} width={'100%'} onSubmit={handleSubmit(handleSignIn)}>
+        <Stack mt={'10'} mb={'4'} spacing={'4'}>
+          <FormControl>
+            <Input
+              {...register('email')}
+              label="SEU E-MAIL"
+              id="email"
+              type={'email'}
+              name="email"
+              error={errors.email}
             />
-          </Flex>
+          </FormControl>
 
-          <Box as={'form'} width={'100%'} onSubmit={handleSubmit(handleSignIn)}>
-            <Stack mt={'10'} mb={'4'} spacing={'4'}>
-              <FormControl>
-                <Input
-                  {...register('email')}
-                  label="SEU E-MAIL"
-                  id="email"
-                  type={'email'}
-                  name="email"
-                  error={errors.email}
-                />
-              </FormControl>
+          <FormControl>
+            <Input
+              {...register('password')}
+              label="SUA SENHA"
+              id="password"
+              type={'password'}
+              name="password"
+              error={errors.password}
+            />
+          </FormControl>
+        </Stack>
 
-              <FormControl>
-                <Input
-                  {...register('password')}
-                  label="SUA SENHA"
-                  id="password"
-                  type={'password'}
-                  name="password"
-                  error={errors.password}
-                />
-              </FormControl>
-            </Stack>
-
-            <Button
-              colorScheme={'purple'}
-              type="submit"
-              size={'lg'}
-              width={'100%'}
-              isLoading={isSubmitting}
-            >
-              Entrar no sistema
-            </Button>
-          </Box>
-        </Flex>
-      </Flex>
+        <Button
+          colorScheme={'purple'}
+          type="submit"
+          size={'lg'}
+          width={'100%'}
+          isLoading={isSubmitting}
+        >
+          Entrar no sistema
+        </Button>
+      </Box>
     </>
   )
 }
