f4c994f421ad37e25a403aed30520d7efc167b24
Dailton Bastos <dailtonbastos@gmail.com>
2022-02-12 22:36:23 -0300

    feat: add modal confirmation

---
+++ src/components/ModalConfirm/index.tsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import { BsXCircle } from 'react-icons/bs'
+
+import {
+  Modal as ChakraModal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Text,
+  Button,
+  Icon,
+  UseDisclosureProps,
+} from '@chakra-ui/react'
+
+interface ModalProps extends UseDisclosureProps {
+  onClose: () => void
+  handleClick: () => void
+  isLoading?: boolean
+}
+
+export const ModalConfirm = ({
+  isOpen = false,
+  onClose,
+  handleClick,
+  isLoading,
+}: ModalProps) => {
+  return (
+    <ChakraModal
+      isCentered
+      isOpen={isOpen}
+      onClose={onClose}
+      motionPreset="slideInBottom"
+    >
+      <ModalOverlay bg="rgba(0,0,0,0.6)" />
+
+      <ModalContent>
+        <ModalHeader
+          alignItems="center"
+          display="flex"
+          justifyContent="center"
+          mt="4"
+        >
+          <Icon as={BsXCircle} color="red.500" h={12} w={12} />
+        </ModalHeader>
+
+        <ModalBody textAlign="center">
+          <Text fontSize="xl" fontWeight="semibold" mb="4">
+            Deseja continuar?
+          </Text>
+          <Text color="gray.500">Esse processo não pode ser desfeito.</Text>
+        </ModalBody>
+
+        <ModalFooter
+          alignItems="center"
+          display="flex"
+          gap={3}
+          justifyContent="center"
+        >
+          <Button colorScheme="gray" w="50%" onClick={onClose}>
+            Cancelar
+          </Button>
+
+          <Button
+            colorScheme="red"
+            w="50%"
+            onClick={handleClick}
+            isLoading={isLoading}
+          >
+            Excluir
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </ChakraModal>
+  )
+}
